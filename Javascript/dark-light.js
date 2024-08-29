232df4fc8f607ff00cea7041cdf57e41a3451aa7
A-Vargas-GP <avarg610@gmail.com>
2024-08-29 18:34:16 -0400

    Adding dark/light option

--- Javascript/dark-light.js
+++ Javascript/dark-light.js
@@ -1,6 +1,41 @@
-function changeDarkLight() {
-    const elem = document.getElementById("DarkLightToggle");
-    val = elem.bootstrapToggle();
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+$(document).ready(function() {
+  $('body').addClass('light-mode');
+
+  $('#dark-light-event').change(
+    function() {
+      if ($(this).prop('checked')) {
+        $('body').addClass('light-mode');
+        $('body').removeClass('dark-mode');
+        // $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+      }
+      else {
+        $('body').addClass('dark-mode');
+        $('body').removeClass('light-mode');
+        // $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+      }
+    })
+});
+
+//  Alternative to above:
+// -------------------------------------------------------------------------
+// $(document).ready(function() {
+//   $('#dark-light-event').change(
+//     function() {
+//       if ($(this).prop('checked')) {
+//         $('body').css(
+//           {
+//             "background-color": "white", 
+//             "color": "black"
+//           });
+//         // $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+//       }
+//       else {
+//         $('body').css(
+//           {
+//             "background-color": "black", 
+//             "color": "white"
+//           });
+//         // $('#console-event').html('Toggle: ' + $(this).prop('checked'))
+//       }
+//     })
+// });
